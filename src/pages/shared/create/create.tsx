aaa8ffc1dad1ba26c10ae641043153bcfe97bd91
Manasina <tianiazymarindrano@gmail.com>
2022-09-12 11:07:59 +0300

    simple feed work 🤫

--- src/pages/shared/create/create.tsx
+++ src/pages/shared/create/create.tsx
@@ -1,13 +1,33 @@
-import { RiSendPlaneFill } from "react-icons/ri";
+import { RiCloseLine, RiSendPlaneFill } from "react-icons/ri";
 import { useForm } from "@mantine/form";
 import { Group, Text, useMantineTheme } from "@mantine/core";
-import { Dropzone, DropzoneProps, IMAGE_MIME_TYPE } from "@mantine/dropzone";
+import { useState } from "react";
+import {
+  Dropzone,
+  DropzoneProps,
+  FileWithPath,
+  IMAGE_MIME_TYPE,
+  MIME_TYPES,
+} from "@mantine/dropzone";
 import "dayjs/locale/fr";
 import { DatePicker } from "@mantine/dates";
-import { IoMdImage } from "react-icons/io";
+import { IoMdClock, IoMdImage } from "react-icons/io";
 import { TimeInput } from "@mantine/dates";
 import { BsCheck2Circle } from "react-icons/bs";
+import { BsDot } from "react-icons/bs";
+import { Select } from "@mantine/core";
+import { facebookUrl } from "../../../types/facebook";
+const hardCoded = {
+  page_id: "110659441785551",
+  access_token:
+    "EAAIpLiWs5qIBAPV16vmRmk32uGGMWPnjacdI15aw2bMmiTBiEUa4a2DKSIvJ68XX1sE31DWx6NB4QZBqtuATdEKv6vxQ0oHgamZAoLsVH0UftDqVgjuKeRmgn2TkMwJ4G8HMWCkyEgqgGNWpOFZBrbG0ODWDq0FOzShaBZAzeQE6U65Y4gWVa2QmAmIksdMZD",
+};
 const CreatePost = (props: Partial<DropzoneProps>) => {
+  const theme = useMantineTheme();
+  const [typeOfPost, setTypeOfPost] = useState<"feed" | "photos" | "videos">(
+    "photos"
+  );
+  const [files, setFiles] = useState<FileWithPath[]>([]);
   const form = useForm({
     initialValues: {
       title: "",
@@ -15,21 +35,63 @@ const CreatePost = (props: Partial<DropzoneProps>) => {
       description: "",
     },
   });
-  const theme = useMantineTheme();
+  const handlePublish = async () => {
+    if (typeOfPost === "feed") {
+      const postUrl = `https://graph.facebook.com/${hardCoded.page_id}/${typeOfPost}?message=${form.values.title}&access_token=${hardCoded.access_token}`;
+      const response = await fetch(postUrl, {
+        method: "POST",
+      });
+      console.log(response);
+    } else {
+      const image = URL.createObjectURL(files[0]);
+      const postUrl = `https://graph.facebook.com/${hardCoded.page_id}/${typeOfPost}?message=${form.values.title}&url=https://images.unsplash.com/photo-1662675120072-bcfb3ceb4460?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60&access_token=${hardCoded.access_token}`;
+      const response = await fetch(postUrl, {
+        method: "POST",
+      });
+      console.log(response);
+    }
+  };
   return (
     <div className="bg-white rounded-xl p-4">
-      <div className="flex items-center justify-between">
-        <h2 className="text-3xl font-semibold text-slate-500 ">
-          Creation d' une poste
-        </h2>
-        <button className="mx-2 my-2 bg-sky-500 transition duration-150 ease-in-out hover:bg-sky-400 rounded text-white px-6 py-2 text-xl font-semibold flex items-center justify-between gap-4">
-          <span>Publier</span>
-          <RiSendPlaneFill className="w-6 h-6" />
-        </button>
-        {/* Code block for primary button ends */}
-      </div>
-      <div className="grid grid-cols-[7fr_4fr] min-h-[76vh] gap-12">
+      <div className="grid grid-cols-[7fr_4fr] gap-12">
         <div>
+          <div className="flex items-center justify-between">
+            <h2 className="text-3xl font-semibold text-slate-500 ">
+              Creation d' une poste
+            </h2>
+            <button
+              className="mx-2 my-2 bg-sky-500 transition duration-150 ease-in-out hover:bg-sky-400 rounded text-white px-6 py-2 text-xl font-semibold flex items-center justify-between gap-4"
+              onClick={handlePublish}
+            >
+              <span>Publier</span>
+              <RiSendPlaneFill className="w-6 h-6" />
+            </button>
+
+            {/* Code block for primary button ends */}
+          </div>
+          <div className="mt-8 flex flex-col w-full">
+            <label
+              htmlFor="about"
+              className="pb-2 font-bold text-gray-800 dark:text-gray-100 text-left text-lg"
+            >
+              Type de poste
+            </label>
+            <Select
+              sx={{
+                maxWidth: 300,
+                fontSize: 600,
+              }}
+              onChange={(value: "feed" | "videos" | "photos") =>
+                setTypeOfPost(value)
+              }
+              data={[
+                { value: "feed", label: "Message" },
+                { value: "videos", label: "Message and Video" },
+                { value: "photos", label: "Message and Photo" },
+              ]}
+            />
+          </div>
+
           <div className="mt-8 flex flex-col w-full">
             <label
               htmlFor="about"
@@ -75,7 +137,11 @@ const CreatePost = (props: Partial<DropzoneProps>) => {
               {...form.getInputProps("description")}
             />
           </div>
-          <div className="flex flex-col my-5">
+          <div
+            className={`flex flex-col my-5 ${
+              typeOfPost === "feed" ? "hidden" : ""
+            }`}
+          >
             <label
               htmlFor="files"
               className="pb-2 font-bold text-gray-800 dark:text-gray-100 text-left text-lg "
@@ -83,10 +149,10 @@ const CreatePost = (props: Partial<DropzoneProps>) => {
               Choisissez des photos ou video
             </label>
             <Dropzone
-              onDrop={(files) => console.log("accepted files", files)}
+              onDrop={setFiles}
               onReject={(files) => console.log("rejected files", files)}
               maxSize={3 * 1024 ** 2}
-              accept={IMAGE_MIME_TYPE}
+              accept={[...IMAGE_MIME_TYPE, MIME_TYPES.webp, MIME_TYPES.mp4]}
               {...props}
             >
               <Group
@@ -133,58 +199,72 @@ const CreatePost = (props: Partial<DropzoneProps>) => {
           <h2 className="text-2xl font-semibold text-slate-500 mb-3">
             Planing
           </h2>
-          <div className="bg-fotsy p-2 flex flex-col gap-5">
-            <div className=" p-4 ">
+          <div className="bg-fotsy p-3 flex flex-col gap-5 rounded-xl">
+            <div className="p-4">
               <div className="flex items-center gap-4">
-                <div>
+                <div className="flex flex-col flex-1">
                   <label
-                    htmlFor="files"
-                    className="pb-2 font-bold text-gray-800 dark:text-gray-100 text-left text-lg "
+                    htmlFor="date"
+                    className="pb-2 font-semibold text-gray-800 dark:text-gray-100 text-left text-lg"
                   >
                     Date optimale
                   </label>
                   <DatePicker locale="fr" defaultValue={new Date()} />
                 </div>
-                <div>
+                <div className="flex flex-col flex-1">
                   <label
-                    htmlFor="files"
-                    className="pb-2 font-bold text-gray-800 dark:text-gray-100 text-left text-lg "
+                    htmlFor="heure"
+                    className="pb-2 font-semibold text-gray-800 dark:text-gray-100 text-left text-lg"
                   >
                     Heure optimale
                   </label>
-                  <TimeInput />
+                  <TimeInput
+                    icon={<IoMdClock size={16} />}
+                    defaultValue={new Date()}
+                  />
                 </div>
               </div>
             </div>
             <div className="p-4 ">
               <div className="flex items-center gap-4">
-                <div>
+                <div className="flex-1 text-orange-500">
                   <label
-                    htmlFor="files"
-                    className="pb-2 font-bold text-gray-800 dark:text-gray-100 text-left text-lg "
+                    htmlFor="status"
+                    className="pb-2 font-semibold text-gray-800 dark:text-gray-100 text-left text-lg"
                   >
                     Etat de validation
                   </label>
+                  <div className="bg-white flex items-center p-3 mt-3">
+                    <BsDot size={30} />
+                    <span></span>
+                    <span className="text-lg">En attente</span>
+                  </div>
                 </div>
-                <div>
+                <div className="flex-1 text-green-500">
                   <label
-                    htmlFor="files"
-                    className="pb-2 font-bold text-gray-800 dark:text-gray-100 text-left text-lg "
+                    htmlFor="publication"
+                    className="pb-2 font-semibold text-gray-800 dark:text-gray-100 text-left text-lg"
                   >
                     Etat de publication
                   </label>
+                  <div className="bg-white flex items-center p-3 mt-3">
+                    <span>
+                      <BsDot size={30} />
+                    </span>
+                    <span className="text-lg">Valide</span>
+                  </div>
                 </div>
               </div>
             </div>
-            <button className="bg-white items-center flex justify-center p-3 w-full gap-4 mb-5">
+            <button className="bg-white items-center flex justify-center p-3 gap-4 mb-5">
               <BsCheck2Circle className="w-8 h-8" />
               <span className="text-xl font-bold">Envoyer publication</span>
             </button>
           </div>
           <div className="flex justify-end">
             <button className="mx-2 my-2 bg-fotsy transition duration-150 ease-in-out hover:bg-fotsy rounded text-slate-400 px-6 py-2 text-xl font-semibold flex items-center justify-between gap-4">
-              <span>Publier</span>
-              <RiSendPlaneFill className="w-6 h-6" />
+              <span>Cancel</span>
+              <RiCloseLine className="w-6 h-6" />
             </button>
             <button className="mx-2 my-2 bg-fotsy transition duration-150 ease-in-out hover:bg-fotsy rounded text-blue-500 px-6 py-2 text-xl font-semibold flex items-center justify-between gap-4">
               <span>Plannifier</span>
--- src/types/facebook.ts
+++ src/types/facebook.ts
@@ -1,69 +1,79 @@
 export type FacebookAuthResponse = {
-  accessToken: string
-  data_access_expiration_time: number
-  expiresIn: number
-  userID: string
-  graphDomain: "facebook"
-  signedRequest: string
-}
+  accessToken: string;
+  data_access_expiration_time: number;
+  expiresIn: number;
+  userID: string;
+  graphDomain: "facebook";
+  signedRequest: string;
+};
 
 export type FacebookStatusResponse = {
-  authResponse: FacebookAuthResponse
-  status: string
-}
+  authResponse: FacebookAuthResponse;
+  status: string;
+};
 
 export type FacebookMe = {
-  name: string
-  about: string
-  email: string
-  birthday: string
-  age_range: string
-  id: string
+  name: string;
+  about: string;
+  email: string;
+  birthday: string;
+  age_range: string;
+  id: string;
   // error:
-}
+};
 
 export type FacebookUserLongLivedToken = {
-  token_type: string
-  access_token: string
-  expires_in: number // in seconds
-}
+  token_type: string;
+  access_token: string;
+  expires_in: number; // in seconds
+};
 
 export type FacebookPage = {
-  name: string
-  access_token: string
-}
+  name: string;
+  access_token: string;
+};
 
-export type FacebookPageList = FacebookPage[]
+export type FacebookPageList = FacebookPage[];
 
 export type FacebookPageTokenRespone = {
   data: {
-    access_token: string
-    category: string
+    access_token: string;
+    category: string;
     category_list: [
       {
-        id: string
-        name: string
+        id: string;
+        name: string;
       }
-    ]
-    name: string
-    id: string
-    tasks: string[]
-  }[]
+    ];
+    name: string;
+    id: string;
+    tasks: string[];
+  }[];
 
   paging: {
     cursors: {
-      before: string
-      after: string
-    }
-  }
-}
+      before: string;
+      after: string;
+    };
+  };
+};
 
 export type RedeemToken = {
-  access_token: string
-  expires_in: number
-  machine_id: string
-}
+  access_token: string;
+  expires_in: number;
+  machine_id: string;
+};
 
 export type FacebookPagePost = {
-  id: string
-}
+  id: string;
+};
+
+export const facebookUrl = (
+  page_id: string,
+  message: string,
+  url: string,
+  access_token: string,
+  type: string
+) => {
+  return `https://graph.facebook.com/${page_id}/${type}?message=${message}&url=${url}&access_token=${access_token}`;
+};
