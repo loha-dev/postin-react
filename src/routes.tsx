5a71dd87414d5e06e399258532dad6995659d067
mianala <mianalaloharano@gmail.com>
2022-09-14 10:46:33 +0300

    added linkedin login

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,14 @@
-import SignIn from "./pages/shared/auth/sign-in";
-import SignUp from "./pages/shared/auth/sign-up";
-import ForgotPassword from "./pages/shared/auth/forgot-password";
-import MainLayout from "./pages/shared/layout/z-layout";
-import Facebook from "./pages/shared/facebook/facebook";
-import Planning from "./pages/shared/planning/planning";
-import Dashboard from "./pages/shared/home/dashboard";
-import Feeds from "./pages/shared/feeds/z-feed";
-import { Navigate } from "@tanstack/react-location";
-import Create from "./pages/shared/create/create";
+import SignIn from "./pages/shared/auth/sign-in"
+import SignUp from "./pages/shared/auth/sign-up"
+import ForgotPassword from "./pages/shared/auth/forgot-password"
+import MainLayout from "./pages/shared/layout/z-layout"
+import Facebook from "./pages/shared/social-network/facebook"
+import Planning from "./pages/shared/planning/planning"
+import Dashboard from "./pages/shared/home/dashboard"
+import Feeds from "./pages/shared/feeds/z-feed"
+import { Navigate } from "@tanstack/react-location"
+import Create from "./pages/shared/create/create"
+import Linkedin from "./pages/shared/social-network/linkedin"
 export const routes = [
   {
     path: "auth",
@@ -35,8 +36,17 @@ export const routes = [
         element: <Dashboard />,
       },
       {
-        path: "facebook",
-        element: <Facebook />,
+        path: "social",
+        children: [
+          {
+            path: "facebook",
+            element: <Facebook />,
+          },
+          {
+            path: "linkedin",
+            element: <Linkedin />,
+          },
+        ],
       },
       {
         path: "planning",
@@ -77,4 +87,4 @@ export const routes = [
       },
     ],
   },
-];
+]
